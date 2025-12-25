c6b54378bc2547fd1120a023c65eda022f3c8d92
xuxiwu <>
2025-12-25 17:58:00 +0800

    fix

---
+++ src/app/advanced-bac-calculator/page.tsx
@@ -0,0 +1,13 @@
+import BacCalculatorDzPage, {
+  generateMetadata as generateLocaleMetadata,
+} from '../[locale]/advanced-bac-calculator/page';
+import { defaultLocale } from '@/lib/i18n';
+
+export async function generateMetadata() {
+  return generateLocaleMetadata({ params: { locale: defaultLocale } });
+}
+
+export default function Page() {
+  return <BacCalculatorDzPage params={{ locale: defaultLocale }} />;
+}
+
