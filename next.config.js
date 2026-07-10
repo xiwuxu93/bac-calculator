fca13ab831ee597b3c107e6a2305fb061815b9f3
mexuxiwu@163.com <773927425@qq.com>
2026-07-10 09:21:10 +0800

    调整

--- next.config.js
+++ next.config.js
@@ -6,6 +6,20 @@ const config = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en/',
+        permanent: true,
+      },
+      {
+        source: '/:path((?!en(?:/|$)|api(?:/|$)|_next(?:/|$)|[^/]+\\.[^/]+$).+)',
+        destination: '/en/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withNextIntl(config);
